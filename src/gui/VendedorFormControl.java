8f9d40a3d4c9160c93f455bcda145491281f1a1f
Luiz Felipe <luizfelipepintorr@gmail.com>
2022-07-20 21:47:41 -0300

    VendedorForm

---
+++ src/gui/VendedorFormControl.java
@@ -0,0 +1,130 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.util.Alertas;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entidades.Vendedor;
+import model.exceptions.ValidationException;
+import model.service.VendedorService;
+
+public class VendedorFormControl implements Initializable {
+	
+	private Vendedor entities;
+	
+	private VendedorService service;
+	
+	private List<DataChengeListener> dataChengeListeners = new ArrayList<>(); 
+	
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML 
+	private Label labelErroName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		if(entities == null) {
+			throw new IllegalStateException("Entities Null");
+		}
+		if(service == null) {
+			throw new IllegalStateException("Service Null");
+		}
+		try {
+			entities = getFormData();
+			service.saveOrUpdate(entities);
+			notifyDataChangeListener();
+			Utils.currentStage(event).close();
+		}catch(DbException e){
+			Alertas.showAlert("Erro ao Salvar o Objeto", null, e.getMessage(), AlertType.ERROR);
+		}catch(ValidationException e) {
+			setErrorMensage(e.getErros());
+		}
+	}
+	
+	private void notifyDataChangeListener() {
+		for(DataChengeListener listener : dataChengeListeners) {
+			listener.onDatachanged();
+		}
+	}
+
+	private Vendedor getFormData() {
+		Vendedor obj = new Vendedor();
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		ValidationException exception = new ValidationException("Validation Error");
+		if(txtName.getText() == null || txtName.getText().trim().equals(""));{
+			exception.addError("name", "Field não pode estar Vazio");
+		}	
+		obj.setNome(txtName.getText());
+		
+		if(exception.getErros().size() == 0) {
+			throw exception;
+		}
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
+	}
+	
+	public void setDeparmantoService(VendedorService service) {
+		this.service = service;
+		
+	}
+	
+	public void setVendedor(Vendedor entities) {
+		this.entities = entities;
+	}
+	
+	public void subscribeDataChangeListener(DataChengeListener listener) {
+		dataChengeListeners.add(listener);
+		
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+			
+	}
+	
+	public void updateFormData() {
+		if(entities == null) {
+			throw new IllegalStateException("Entities Null");
+		}
+		txtId.setText(String.valueOf(entities.getId()));
+		txtName.setText(entities.getNome());
+	}
+	
+	private void setErrorMensage(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("name")) {
+			labelErroName.setText(errors.get("name"));
+		}
+	}
+	
+	
+}
--- src/gui/VendedorListControle.java
+++ src/gui/VendedorListControle.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -15,7 +16,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -23,6 +26,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entidades.Vendedor;
 import model.service.VendedorService;
@@ -105,7 +110,7 @@ public class VendedorListControle implements Initializable, DataChengeListener {
 	}
 
 	public void createDialogoForm(Vendedor obj, String absoluteName, Stage parentStage) {
-		/*try {
+		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
 
@@ -125,7 +130,7 @@ public class VendedorListControle implements Initializable, DataChengeListener {
 
 		} catch (IOException e) {
 			Alertas.showAlert("IO Exception", "Erro Load View", e.getMessage(), AlertType.ERROR);
-		} */
+		} 
 	}
 
 	@Override
